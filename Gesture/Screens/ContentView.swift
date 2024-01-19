e5d1049c30b90123c8d46361b467c774c65672c7
Abhyuday Reddy Nandikonda <abhyudayreddy1534@gmail.com>
2024-01-18 21:18:54 -0600

    Drag gesture, Scale up n down, reset to original size added. Overlay views added to show more details axees wise

--- Gesture/Screens/ContentView.swift
+++ Gesture/Screens/ContentView.swift
@@ -11,10 +11,24 @@ struct ContentView: View {
 //    MARK: Property
     @State private var isAnimating: Bool = false
     @State private var imageScale: CGFloat = 1
+    @State private var imageOffset: CGSize = .zero
+    
+//    MARK: Function
+    
+    func resetImageState() {
+        return withAnimation(.spring()) {
+            imageScale = 1
+            imageOffset = .zero
+        }
+    }
+//    MARK: Content
     
     var body: some View {
         NavigationStack {
             ZStack(content: {
+                
+                Color(.clear)
+                
 //                MARK: Page image
                 Image("magazine-front-cover")
                     .resizable()
@@ -24,6 +38,7 @@ struct ContentView: View {
                     .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
                     .opacity(isAnimating ? 1 : 0)
                     .animation(.linear(duration: 1), value: isAnimating)
+                    .offset(x: imageOffset.width, y: imageOffset.height)
                     .scaleEffect(imageScale)
 //                Tap gesture // count 2 means double tap
                     .onTapGesture(count: 2, perform: {
@@ -33,11 +48,22 @@ struct ContentView: View {
                             }
                         }
                         else {
-                            withAnimation(.spring()) {
-                                imageScale = 1
-                            }
+                            resetImageState()
                         }
                     })
+                    .gesture(
+                        DragGesture()
+                            .onChanged({ value in
+                                withAnimation(.linear(duration: 1)) {
+                                    imageOffset = value.translation
+                                }
+                            })
+                            .onEnded({ _ in
+                                if imageScale <= 1 {
+                                    resetImageState()
+                                }
+                            })
+                    )
             })//: Zstack
             .navigationTitle("Pinch & Zoom")
             .navigationBarTitleDisplayMode(.inline)
@@ -46,6 +72,66 @@ struct ContentView: View {
                     isAnimating = true
                 }
             })
+//            MARK: Info panel
+            .overlay(alignment: .top, content: {
+                InfoPanelView(scale: imageScale, offset: imageOffset)
+                    .padding(.horizontal)
+                    .padding(.top, 30)
+                
+            })
+            
+//            MARK: Controls
+            .overlay(alignment: .bottom) {
+                Group {
+                    HStack {
+//                        Scale down
+                        Button(action: {
+                            withAnimation(.spring()) {
+                                if imageScale > 1 {
+                                    imageScale -= 1
+                                    
+                                    if imageScale <= 1 {
+                                        resetImageState()
+                                    }
+                                }
+                                
+                            }
+                        }, label: {
+                            ControlImageView(icon: "minus.magnifyingglass")
+                        })
+//                        reset
+                        Button(action: {
+                            withAnimation(.spring()) {
+                                resetImageState()
+                                
+                            }
+                        }, label: {
+                            ControlImageView(icon: "arrow.up.left.and.down.right.magnifyingglass")
+                        })
+//                        scale up
+                        Button(action: {
+                            withAnimation(.spring()) {
+                                if imageScale < 5 {
+                                    imageScale += 1
+                                    
+                                    if imageScale > 5 {
+                                        imageScale = 5
+                                    }
+                                }
+                                
+                            }
+                        }, label: {
+                            ControlImageView(icon: "plus.magnifyingglass")
+                        })
+                    }
+                    .padding(EdgeInsets(top: 12, leading: 20, bottom: 12, trailing: 20))
+                    .background(.ultraThinMaterial)
+                    .clipShape(RoundedRectangle(cornerRadius: 12))
+                    .opacity(isAnimating ? 1 : 0)
+                }
+                .padding(.bottom, 30)
+            }
+            
         }//: Navigation stack
         
     }
