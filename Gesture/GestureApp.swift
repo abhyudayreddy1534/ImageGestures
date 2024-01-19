e93db819156cc0a125c951e743c563a1735a9b86
Abhyuday Reddy Nandikonda <abhyudayreddy1534@gmail.com>
2024-01-18 18:17:41 -0600

    Initial Commit

---
+++ Gesture/GestureApp.swift
@@ -0,0 +1,17 @@
+//
+//  GestureApp.swift
+//  Gesture
+//
+//  Created by Sravanthi Chinthireddy on 18/01/24.
+//
+
+import SwiftUI
+
+@main
+struct GestureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
